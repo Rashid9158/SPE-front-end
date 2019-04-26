419c6a7c96f9321560f7ad6d7ed332c27e0fabb1
Rashid9158 <mneyaz96@gmail.com>
2019-04-26 23:04:04 +0530

    Sample test case

--- app/src/test/java/com/example/packagedelivery/ExampleUnitTest.java
+++ app/src/test/java/com/example/packagedelivery/ExampleUnitTest.java
@@ -1,17 +1,36 @@
 package com.example.packagedelivery;
 
+import android.content.Intent;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 
 import static org.junit.Assert.*;
+import static org.robolectric.Shadows.shadowOf;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void clickingLogin_shouldStartLoginActivity() {
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        activity.findViewById(R.id.newPack).performClick();
+
+        Intent expectedIntent = new Intent(activity, Detail.class);
+        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
 }
